c723faf29655f96456d6cc084c6a0e79437794d2
dfoxs353 <58122426+dfoxs353@users.noreply.github.com>
2024-01-03 17:08:02 +0300

    feat: add RoomEvent and RoomViewState

---
+++ Mobile/app/src/main/java/com/example/toaudio/ui/screens/room/models/RoomEvent.kt
@@ -0,0 +1,4 @@
+package com.example.toaudio.ui.screens.room.models
+
+sealed class RoomEvent {
+}
---
+++ Mobile/app/src/main/java/com/example/toaudio/ui/screens/room/models/RoomViewState.kt
@@ -0,0 +1,5 @@
+package com.example.toaudio.ui.screens.room.models
+
+data class RoomViewState(
+    val roomId: String,
+)
